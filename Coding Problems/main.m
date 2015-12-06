39db18e1ef75faef66f444aaac1997d65bf6c6c5
Ecil Teodoro <ecil.teodoro@gmail.com>
2015-12-06 13:22:03 -0800

    Initial Commit

---
+++ Coding Problems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Coding Problems
+//
+//  Created by Ecil Teodoro on 12/6/15.
+//  Copyright © 2015 Mobmee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
